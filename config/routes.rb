12346d37e5cd95fe6443cef205a78f3a273db311
Hara Tomoka <pixieyunomi@gmail.com>
2020-08-31 08:37:32 +0900

    add users show

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+	root 'homes#top'
   devise_for :users
-  root 'homes#top'
+  get 'homes' => 'homes#about'
+  resources :users, only: [:show, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
