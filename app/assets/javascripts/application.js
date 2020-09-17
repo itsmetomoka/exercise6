2327e30b4ed64e2504d76c70d646a50caa22e0fb
Hara Tomoka <pixieyunomi@gmail.com>
2020-09-17 21:38:25 +0900

    finish

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,4 @@
+
 // This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
 //
@@ -12,7 +13,7 @@
 //
 //= require rails-ujs
 //= require turbolinks
-//= require jquery3
+//= require jquery
 //= require popper
 //= require bootstrap-sprockets
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   devise_for :users
   get 'home/about' => 'homes#about'
   resources :users, only: [:show, :edit, :update, :create, :index]
-  resources :books, only: [:new, :create, :index, :show, :destroy,:edit,:update]
+  resources :books do
+  	resource :favorites, only: [:create, :destroy]
+  	resources :book_comments, only: [:create, :destroy]
+ 	end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
