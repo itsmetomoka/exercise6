d8a945dbe7de664121f8aa30753d3219bfcca83d
Hara Tomoka <pixieyunomi@gmail.com>
2020-09-18 18:43:28 +0900

    add relationship

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,17 @@ Rails.application.routes.draw do
 	root 'homes#top'
   devise_for :users
   get 'home/about' => 'homes#about'
-  resources :users, only: [:show, :edit, :update, :create, :index]
+
   resources :books do
   	resource :favorites, only: [:create, :destroy]
   	resources :book_comments, only: [:create, :destroy]
  	end
+
+ 	resources :users do
+    member do
+     get :following, :followers
+    end
+  end
+  resources :relationships, only: [:create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
